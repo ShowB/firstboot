4adca11885c35bb1438ca9ef417c1269fc849521
ShowB <seopvluh@gmail.com>
2025-09-25 00:34:49 +0900

    fix: NonNullApi 관련 워닝 보완

--- src/main/java/com/showb/firstboot/configs/security/JwtAuthenticationFilter.java
+++ src/main/java/com/showb/firstboot/configs/security/JwtAuthenticationFilter.java
@@ -12,6 +12,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -32,7 +33,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
 
         if (!StringUtils.hasText(token)) {
